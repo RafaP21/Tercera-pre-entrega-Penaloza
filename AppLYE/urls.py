56995c3469ccb600c0988ca4c2cf3209a2998f0c
Axl R. Penaloza O <penalozarafael21@gmail.com>
2023-09-03 21:21:17 -0300

    agregados los templates y el archivo css(sin modificar)

--- AppLYE/urls.py
+++ AppLYE/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from AppLYE.views import *
 
 urlpatterns = [
-   
+ path('', inicio) ,
+ path('cliente/', cliente) ,
+ path('empleado/', empleado) ,
+ path('stock/', stock) ,
+ path('lista_stock/', lista_stock) ,
+
 ]
--- AppLYE/views.py
+++ AppLYE/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render
 from .models import *
+from django.http import HttpResponse, HttpRequest
 
-def empleado (erq, nombre,cargo, sueldo):
-    empleado= Empleado(nombre = nombre, cargo = cargo , sueldo = sueldo)
-    empleado.save()
+# def empleado (erq, nombre,cargo, sueldo):
+#     empleado= Empleado(nombre = nombre, cargo = cargo , sueldo = sueldo)
+#     empleado.save()
 
     
-def cliente (req, nombre, producto,dni):
-    cliente = Cliente(nombre = nombre, producto = producto , dni = dni)
-    cliente.save()
+# def cliente (req, nombre, producto,dni):
+#     cliente = Cliente(nombre = nombre, producto = producto , dni = dni)
+#     cliente.save()
 
 
-def stock (req, nombre, cantidad, precio):
-    stock = Stock (nombre= nombre, cantidad = cantidad, precio = precio)
-    stock.save()
+# def stock (req, nombre, cantidad, precio):
+#     stock = Stock (nombre= nombre, cantidad = cantidad, precio = precio)
+#     stock.save()
+
+def inicio (req):
+    return render(req, "inicio.html")
+
+def empleado (req):
+    return render(req, "empleado.html")
+
+def cliente (req):
+    return render(req, "cliente.html")
+
+def stock (req):
+    return render(req, "stock.html")
+
+def lista_stock(req):
+    lista = Stock.objects.all()
+    return render(req, "lista_stock.html", {"lista_stock" : lista})
